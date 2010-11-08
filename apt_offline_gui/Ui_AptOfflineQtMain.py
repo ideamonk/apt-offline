4f38f89ac1b80e545366d36d7e7b1e1c7ccfd54b
Ritesh Raj Sarraf <rrs@researchut.com>
2010-11-08 16:34:10 +0530

    update .py from .ui files

--- apt_offline_gui/Ui_AptOfflineQtMain.py
+++ apt_offline_gui/Ui_AptOfflineQtMain.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'AptOfflineQtMain.ui'
 #
-# Created: Sun Nov  7 20:54:50 2010
+# Created: Mon Nov  8 16:33:02 2010
 #      by: PyQt4 UI code generator 4.7.3
 #
 # WARNING! All changes made in this file will be lost!
